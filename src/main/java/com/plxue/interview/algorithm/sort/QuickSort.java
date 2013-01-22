a42a6bcc2dc93e27787167e8bc32086a19ad2fd6
piaolingxue <piaolingxue305@gmail.com>
2013-01-22 14:30:24 +0800

    add quick sort algorithm.

---
+++ src/main/java/com/plxue/interview/algorithm/sort/QuickSort.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.plxue.interview.algorithm.sort;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * QuickSort (快速排序)
+ * 
+ * @author libin
+ * 
+ */
+public class QuickSort {
+	private static Logger LOG = LoggerFactory.getLogger(QuickSort.class);
+
+	public static void quickSort(int[] data, int start, int end) {
+		if (start < end) {
+			int i = start;
+			int j = end;
+			int num = data[i];
+			while (i != j) {
+				for (; j > i; j--) {
+					if (data[j] <= num) {
+						data[i++] = data[j];
+						break;
+					}
+				}
+				for (; i < j; i++) {
+					if (data[i] > num) {
+						data[j--] = data[i];
+						break;
+					}
+				}
+			}
+			data[i] = num;
+			LOG.debug(String.format("tmp:%s", Arrays.toString(data)));
+			quickSort(data, start, i - 1);
+			quickSort(data, i + 1, end);
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] data = new int[] { 9, 8, 7, 4, 5, 4, 3, 2, 1 };
+		quickSort(data, 0, data.length - 1);
+		LOG.info(String.format("result:%s", Arrays.toString(data)));
+	}
+
+}
